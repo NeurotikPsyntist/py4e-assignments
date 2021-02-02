78171e0beae01bd54cef15c54a73c173966456c1
NeurotikPsyntist <76664058+NeurotikPsyntist@users.noreply.github.com>
2021-02-02 16:03:36 -0700

    create class roster database

--- rosterdb.py
+++ rosterdb.py
@@ -29,7 +29,7 @@ cur.executescript('''
 ''')
 
 fname = input('Enter file name: ')
-if len(fname) < 1 : fname = 'roster_data.json'
+if len(fname) < 1 : fname = 'roster_data_sample.json'
 str_data = open(fname).read()
 json_data = json.loads(str_data)
 #print (json_data)
